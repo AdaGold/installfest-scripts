aa3b6f3c6cd3f5bb1d354d8ea2a1a2c906ffbcfb
char <charmara.mays@adadevelopersacademy.org>
2023-01-09 12:46:26 -0800

    add script

---
+++ installfest_pcl.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Below is a script used at Ada to install needed software and some optional packages (Firefox Chrome etc).
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Then install python
+brew install python
+
+# Make it run Python3 when the user types "python"
+echo "alias python=python3" >> ~/.zshrc
+echo "alias python=python3" >> ~/.profile
+
+# Make it run pip3 when the user types "pip"
+echo "alias pip=pip3" >> ~/.zshrc
+echo "alias pip=pip3" >> ~/.profile
+
+# Get the updated path for homebrew
+source ~/.zshrc
+source ~/.profile
+
+# Set up git
+brew install git
+
+# Read in their name & email address
+echo ""
+IFS= read -r -p "Enter your name: " studentname
+
+echo ""
+echo "Please enter the email address you use with github"
+read studentemail
+
+git config --global user.name "$studentname"
+git config --global user.email "$studentemail"
+git config --global core.ignorecase false
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global push.default current
+git config --global pull.rebase false
+git config --global core.editor "code --wait"
+
+echo "Verification:  This should print your name and email"
+git config --get user.name
+git config --get user.email
+
+# Install Node
+brew install node
+
+# Install VS Code
+echo "Would you like me to install VS Code? Y/N"
+read yesno
+if [ $yesno = "Y" ] || [ $yesno = "y" ]
+then
+  brew install --cask visual-studio-code
+fi
+
+# Install Firefox & Chrome
+echo "Would you like me to install Firefox? Y/N"
+read yesno
+if [ $yesno = "Y" ] || [ $yesno = "y" ]
+then
+  brew install --cask firefox
+fi
+
+echo "Would you like me to install Google Chrome? Y/N"
+read yesno
+if [ $yesno = "Y" ] || [ $yesno = "y" ]
+then
+  brew install --cask google-chrome
+fi
+
+# Install Slack
+echo "Would you like me to install Slack for Mac? Y/N"
+read yesno
+if [ $yesno = "Y" ] || [ $yesno = "y" ]
+then
+  brew install --cask slack
+fi
+
+echo "Installation complete!"
+
+exit
